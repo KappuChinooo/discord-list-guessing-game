f1483430b4d0c8cfdeb86934bee319e2eb49bcab
KappuChinooo <KappuChinooo@gmail.com>
2025-10-24 00:06:04 +0700

    add list gen test

--- discord_bot/game_cog.py
+++ discord_bot/game_cog.py
@@ -1,14 +1,16 @@
 from nextcord.ext import commands
 from nextcord import Interaction, SlashOption
 import nextcord
+from game.game import GameSession
 from game.state import ChannelStateManager
-
+from discord_bot.ui.embeds import make_game_embed
+from discord_bot.ui.start_game_view import StartGameView
+import random
 
 class GameCog(commands.Cog):
     def __init__(self, bot, state_manager: ChannelStateManager):
         self.bot = bot
         self.state_manager = state_manager
-
 
 
     @nextcord.slash_command(name="start", description="Start a new game")
@@ -21,12 +23,14 @@ class GameCog(commands.Cog):
                              choices=["general", "anime", "characters"],
                              default="general")):
         channel_id = interaction.channel.id
-        if self.state_manager.get_game(channel_id):
+        if self.state_manager.get_game(channel_id=channel_id):
             await interaction.response.send_message("Game already active.", ephemeral=True)
             return
-        await self.state_manager.start_game(channel_id, mode, favourites_number, hated_number)
-        await interaction.response.send_message("Game started. Do /submit_list to enter to lists")
-
+        game = await self.state_manager.start_game(channel_id, mode, favourites_number, hated_number)
+        embed = await make_game_embed([])
+        view = StartGameView(interaction.user.id, game)
+        msg = await interaction.response.send_message(embed=embed, view=view)
+        view.message = msg
 
 
     @nextcord.slash_command(name="end", description="Ends the ongoing game")
@@ -39,13 +43,17 @@ class GameCog(commands.Cog):
         await interaction.response.send_message("Game ended.")
 
 
-
-    @nextcord.slash_command(name="submit_list", description="Submits your lists for ongoing game")
-    async def submit_list(self, interaction: Interaction):
+    @nextcord.slash_command(name="generate_list", description="testing")
+    async def gen_list(self, interaction: Interaction):
         channel_id = interaction.channel.id
-        if self.state_manager.get_game(channel_id) is None:
-            await interaction.response.send_message("There is no active game in this channel.", ephemeral=True)
+        if self.state_manager.get_game(channel_id=channel_id) is None:
+            await interaction.response.send_message("no game", ephemeral=True)
             return
-        game = self.state_manager.get_game(channel_id)
-        
-        pass
+        game: GameSession = self.state_manager.get_game(channel_id)
+        for player in range(4):
+            for _ in range(3):
+                await game.submit_entry(player, str(random.randint(0, 20)), True)
+                await game.submit_entry(player, str(random.randint(0, 20)), False)
+        await interaction.response.send_message("done", ephemeral=True)
+
+
